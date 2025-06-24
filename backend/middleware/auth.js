432ac5d244435f790aa080ffd6cd550338eaba85
sanket <sanketyelugotla123@gmail.com>
2025-06-24 09:41:15 +0530

    Added role middleware and minor changes in auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,42 @@
-const jwt = require('jsonwebtoken');
+const { verifyToken } = require('../config/jwt');
+const AppError = require('../utils/appError');
 
-const verifyToken = (req, res, next) => {
-    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
-
-    if (!token) {
-        return res.status(401).json({ message: 'Authentication required' });
-    }
-
+const protect = async (req, res, next) => {
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        // 1) Getting token and check if it's there
+        let token;
+        if (
+            req.headers.authorization &&
+            req.headers.authorization.startsWith('Bearer')
+        ) {
+            token = req.headers.authorization.split(' ')[1];
+        } else if (req.cookies.token) {
+            token = req.cookies.token;
+        }
+
+        if (!token) {
+            return next(
+                new AppError('You are not logged in! Please log in to get access.', 401)
+            );
+        }
+
+        // 2) Verification token
+        const decoded = await verifyToken(token);
+
+        // 3) Check if user still exists
+        const currentUser = await User.findById(decoded.id);
+        if (!currentUser) {
+            return next(
+                new AppError('The user belonging to this token does no longer exist.', 401)
+            );
+        }
+
+        // GRANT ACCESS TO PROTECTED ROUTE
+        req.user = currentUser;
         next();
     } catch (err) {
-        return res.status(401).json({ message: 'Invalid token' });
+        next(err);
     }
 };
 
-module.exports = { verifyToken };
+module.exports = protect;
---
+++ backend/middleware/role.js
@@ -0,0 +1,15 @@
+const AppError = require('../utils/appError');
+
+const restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new AppError('You do not have permission to perform this action', 403)
+            );
+        }
+
+        next();
+    };
+};
+
+module.exports = restrictTo;
